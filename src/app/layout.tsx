ff71b5ca8ebb6a0335f6024eae842c9565b74070
Miguel Victor Lopes Alves <miguelvictor93alves@gmail.com>
2026-04-07 22:12:20 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@v

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Qual Princesa É Essa?",
@@ -35,7 +34,6 @@ export default function RootLayout({
           </footer>
         </div>
         <Toaster />
-        <Analytics />
       </body>
     </html>
   );
