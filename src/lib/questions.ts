1d1518e5b03967fb6b273f97d079d8f4ef4e104b
Miguel Victor Lopes Alves <miguelvictor93alves@gmail.com>
2026-04-08 00:48:51 +0000

    mude essa frase para outra frase seguindo a mesma ideia, mas falando per

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -32,7 +32,7 @@ export const questions: QuizQuestion[] = [
   {
     princess: "???",
     imageUrl: "https://preview.redd.it/did-you-know-typing-this-one-symbol-in-text-chat-makes-v0-v57n9xlcf52g1.jpeg?auto=webp&s=f157aa940184a93194deb909f89a1c1129e5aab7",
-    question: "Dizem que a beleza dela encanta o mundo inteiro. Qual é a princesa mais deslumbrante de todas?",
+    question: "Todos dizem que ela é a mais perfeita e maravilhosa de todas as princesas. De quem estamos falando?",
     options: ["Ariel", "Moana", "Elsa", "Alicia"],
     correct: "Alicia",
     isSpecial: true,
